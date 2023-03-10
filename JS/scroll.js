28d605351d52c5a526d8304d0e7f38d0fb85973f
aravind-prabakar962 <aravindprabakar962@gmail.com>
2023-03-10 19:16:23 +0530

    new loan page

--- JS/scroll.js
+++ JS/scroll.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.documentElement.scrollTop > 1400) {
+  if (document.documentElement.scrollTop > 1500) {
     document.getElementById("myBtn").style.display = "block";
   } else {
     document.getElementById("myBtn").style.display = "none";
