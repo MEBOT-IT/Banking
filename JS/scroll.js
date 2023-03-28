601f424fcdadddb567ddd14f7c1408bbc66f3efe
RanjaniKamaraj <sivaranjani3856@gmail.com>
2023-03-29 00:43:15 +0530

    completed

--- JS/scroll.js
+++ JS/scroll.js
@@ -15,16 +15,25 @@ window.scrollTo({ top: 0, behavior: "smooth" });
 //TIME SHOWER:
 
 function digi(){
+
   var date=new Date();
   var hrs=date.getHours();
   var min=date.getMinutes();
   var sec=date.getSeconds();
-  
-  
 
+  if (hrs < 10){
+    hrs = "0" + hrs;
+  }
+  if (min < 10){
+    min = "0" + min;
+  }
+  if(sec < 10){
+    sec = "0" + sec;
+  }
+  
   document.getElementById("hours").innerHTML=hrs;
   document.getElementById("minutes").innerHTML=min;
   document.getElementById("seconds").innerHTML=sec;
 
 }
-  setInterval(digi)
+setInterval(digi)
